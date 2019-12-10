4c9e4819e13426aa3f2396b3fd397c4b065a5b53
MySecondLanguage <mmamunism@gmail.com>
2019-12-10 04:49:42 -0500

    Commented out api helper

---
+++ client/apiHelper.js
@@ -0,0 +1,10 @@
+// import axios from '../client/utils/axios'; // Configured axios
+
+// const apiHelper = {
+//     getQuestionList: () => axios.get(`question-list/`),
+
+//     // login: (data) => axios.post(`account/login/`, data),
+//     // verifyToken: (data) => axios.post(`account/verify/`, data),
+// };
+
+// export default apiHelper;
