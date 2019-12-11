a80f6ed0d197d0efa5c0f2d7b6f3b27fdb2ba89f
MySecondLanguage <mmamunism@gmail.com>
2019-12-10 21:43:19 -0500

    Excluded resutl question

--- backend/examapi/views.py
+++ backend/examapi/views.py
@@ -13,6 +13,8 @@ from exam.models import (
     Question,
     QuestionGroup
 )
+
+from result.models import Result
 from examapi.serializers import (
     QuestionSerializer
 )
@@ -21,8 +23,16 @@ class QuestionListView(RetrieveAPIView):
     serializer_class = QuestionSerializer
     
     def get_object(self):
+        result = Result.objects.filter(
+            user=self.request.user
+        ).values(
+            'question__id'
+        )
+
         queryset = Question.objects.filter(
             exam=self.request.current_exam
+        ).exclude(
+            result__question__id__in=[result]
         ).first()
         return queryset
 
--- backend/result/admin.py
+++ backend/result/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from result.models import Result
+
+admin.site.register(Result)
