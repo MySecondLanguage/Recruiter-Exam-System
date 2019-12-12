ed71eb4623347a6a1675f80ac43b2540df136058
MySecondLanguage <mmamunism@gmail.com>
2019-12-12 05:11:41 -0500

    Converted time into flor second

--- backend/examapi/serializers.py
+++ backend/examapi/serializers.py
@@ -21,10 +21,19 @@ class ChoiceSerializer(ModelSerializer):
 # Seriliaze profile model
 class QuestionSerializer(ModelSerializer):
     choices = ChoiceSerializer(read_only=True, many=True)
+    total_duration = serializers.SerializerMethodField()
     class Meta:
         model = Question
     
         fields = '__all__'
+
+
+    def get_total_duration(self, obj):
+        total_duration = str(obj.total_duration)
+        h, m, s = total_duration.split(':')
+        return int(h) * 3600 + int(m) * 60 + int(s)
+
+    
 
 class ResultCreateSerializer(ModelSerializer):
     class Meta:
