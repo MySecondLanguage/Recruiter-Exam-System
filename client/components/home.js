70314b240eda5f80f970626ba4562de33c0c0f4f
MySecondLanguage <mmamunism@gmail.com>
2019-12-12 06:56:11 -0500

    calculated second to milisecond

--- client/components/home.js
+++ client/components/home.js
@@ -20,6 +20,7 @@ class Home extends React.Component {
     }
 
     continueOnTimeOut = (time) => {
+        time = parseInt(time) * 1000
         setTimeout(() => {
             // this.inputElement.click();
             console.log('reloaded', time)
