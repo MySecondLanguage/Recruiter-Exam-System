15f1fc98b61491da1bff513641eaeb0ac838847a
MySecondLanguage <mmamunism@gmail.com>
2019-12-13 01:37:30 -0500

    imported all the model field

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -3,6 +3,20 @@ from django.shortcuts import (
     HttpResponse
 )
 
+from exam.models import (
+    Choice,
+    Exam,
+    Question,
+    QuestionChoice,
+    QuestionGroup
+)
+
+from result.models import Result
+
+from account.models import (
+    Profile
+)
+
 def home(request):
     return HttpResponse('hello world')
 
