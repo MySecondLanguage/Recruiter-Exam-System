3e45ebf46ac8e93941d84feb953649a3fd38a430
MySecondLanguage <mmamunism@gmail.com>
2019-12-14 10:34:07 -0500

    added dynamic topic select

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -24,27 +24,30 @@ def home(request):
     return render(request, 'dashboard/index.html')
 
 def question_pool(request):
-    topic_to_query=["css", "java", "python"]
+    context = {}
+    topic = Topic.objects.all()
+    context['topics'] = topic
 
-    questions_by_topic = Topic.objects.filter(
-    name__in=topic_to_query
-    ).prefetch_related(
-        'questions_by_topic'
-    )
+    print(request.GET.keys(), '-------------------------------------')
+    if request.GET:
+        topic_to_query=list(request.GET.keys())
+        questions_by_topic = Topic.objects.filter(
+        name__in=topic_to_query
+        ).prefetch_related(
+            'questions_by_topic'
+        )
 
-    # """
-    # WE SHOULD CONSTRUCT RESULT LIKE THIS BELOW IF YOU WANT TO QUERY IN TEMPLATE LABEL
-    # {% for question in topic.questions_by_topic.all %}
-    # """
-    # result = {
-    #     t.name: [{'id': q.id, 'title': q.title} for q in t.questions_by_topic.all()]
-    #     for t in questions_by_topic
-    # }
+        # """
+        # WE SHOULD CONSTRUCT RESULT LIKE THIS BELOW IF YOU WANT TO QUERY IN TEMPLATE LABEL
+        # {% for question in topic.questions_by_topic.all %}
+        # """
+        # result = {
+        #     t.name: [{'id': q.id, 'title': q.title} for q in t.questions_by_topic.all()]
+        #     for t in questions_by_topic
+        # }
 
-    context = {
-        'topics': questions_by_topic
-    }
-    
+        context['topic_question'] = questions_by_topic
+        
     return render(request, 'dashboard/question-pool.html', context)
 
 
