94dfa11324619897c3f36903dfd544c8f480bc3d
MySecondLanguage <mmamunism@gmail.com>
2019-12-14 08:14:21 -0500

    added related name in question model

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import (
     HttpResponse
 )
 
+from django.db.models import Count
+
 from exam.models import (
     Choice,
     Exam,
     Question,
     QuestionChoice,
-    QuestionGroup
+    QuestionGroup,
+    Topic
 )
 
 from result.models import Result
@@ -21,5 +24,14 @@ def home(request):
     return render(request, 'dashboard/index.html')
 
 def question_pool(request):
+    # topic_to_query=["css", "java"]
+
+    qs = Topic.objects.filter(
+    name__in=['css', 'java']
+    ).prefetch_related('questions_by_topic')
+
+    print(qs, '---------------------------------------------------')
     return render(request, 'dashboard/question-pool.html')
+
+
 
