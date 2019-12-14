8281ccadbc1d52d94d7d979aabee4630ee431d9d
MySecondLanguage <mmamunism@gmail.com>
2019-12-14 08:39:08 -0500

    Showed all the question

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -24,14 +24,28 @@ def home(request):
     return render(request, 'dashboard/index.html')
 
 def question_pool(request):
-    # topic_to_query=["css", "java"]
+    topic_to_query=["css", "java", "python"]
 
-    qs = Topic.objects.filter(
-    name__in=['css', 'java']
-    ).prefetch_related('questions_by_topic')
+    questions_by_topic = Topic.objects.filter(
+    name__in=topic_to_query
+    ).prefetch_related(
+        'questions_by_topic'
+    )
 
-    print(qs, '---------------------------------------------------')
-    return render(request, 'dashboard/question-pool.html')
+    # """
+    # WE SHOULD CONSTRUCT RESULT LIKE THIS BELOW IF YOU WANT TO QUERY IN TEMPLATE LABEL
+    # {% for question in topic.questions_by_topic.all %}
+    # """
+    # result = {
+    #     t.name: [{'id': q.id, 'title': q.title} for q in t.questions_by_topic.all()]
+    #     for t in questions_by_topic
+    # }
+
+    context = {
+        'topics': questions_by_topic
+    }
+    
+    return render(request, 'dashboard/question-pool.html', context)
 
 
 
