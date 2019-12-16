341799e8bebce6550bef74a7c77ded1b3da00771
MySecondLanguage <mmamunism@gmail.com>
2019-12-16 02:17:48 -0500

    added result summery

--- backend/examapi/views.py
+++ backend/examapi/views.py
@@ -77,3 +77,4 @@ class CreateResult(CreateAPIView):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        
--- backend/result/models.py
+++ backend/result/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from exam.models import Question
+from exam.models import (
+    Question,
+    Exam,
+)
 
 
 class Result(models.Model):
@@ -21,3 +24,21 @@ class Result(models.Model):
         null=True,
         blank=True
     )
+
+    def __str__ (self):
+        return self.user.question.title
+
+class ResultSummery(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    is_exam_completed = models.BooleanField(default=False)
+    exam = models.OneToOneField(
+        Exam,
+        on_delete=models.CASCADE
+    )
+    total_marks = models.IntegerField()
+
+    def __str__ (self):
+        return self.exam.name
